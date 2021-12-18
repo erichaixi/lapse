6f4738a21704a9ac6cd8c3380cc4ded8040dbf1f
Eric Xi <43021670+erichaixi@users.noreply.github.com>
2021-12-18 12:35:32 -0600

    Remove v1 files

--- Time Lapse/run.ps1
+++
@@ -1,6 +0,0 @@
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
-
-Set-Location $dir
-
-python lapse.py
--- run.ps1
+++
@@ -1,9 +0,0 @@
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
-
-Set-Location $dir
-
-conda activate lapse
-python lapse.py
-
-pause
